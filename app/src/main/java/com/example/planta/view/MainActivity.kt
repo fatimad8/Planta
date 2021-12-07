738b727ad6f1fc5542633ca2a5df5c499667e5d8
Fatimah Aldagdoug <aldagdougfatm@gmail.com>
2021-12-07 11:20:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/planta/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.planta.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planta.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
